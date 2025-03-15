21f707ba9a661ed0277e49534ab9e790edc0bd3d
sufyan20033 <sufyanlehri10@gmail.com>
2025-03-16 00:56:16 +0500

    Added like button
    Added a like button so that user likes any word is he wants

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -52,6 +52,13 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -62,6 +69,16 @@ class MyHomePage extends StatelessWidget {
             Row(
               mainAxisSize: MainAxisSize.min,   
               children: [
+
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+
                 ElevatedButton(
                   onPressed: () {
                     appState.getNext();
