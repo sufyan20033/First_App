4b02e4ef9f3a525879925049be85206ac5168c8d
sufyan20033 <sufyanlehri10@gmail.com>
2025-03-15 23:53:34 +0500

    Update card
    Painted card with a richer color. And because it's always a good idea to keep a consistent color scheme, use the app's Theme to choose the color

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -69,7 +69,9 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context); 
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Text(pair.asLowerCase),
