cf9bab082af1ae113f38dc5f65e2565941ed041b
sufyan20033 <sufyanlehri10@gmail.com>
2025-03-15 22:38:19 +0500

    Changes the string
    Added new string

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -37,7 +37,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('New Flutter App'),
           Text(appState.current.asLowerCase),
         ],
       ),
