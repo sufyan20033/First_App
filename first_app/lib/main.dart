d42a5a67d3db09526e137e14a34803f8db417a73
sufyan20033 <sufyanlehri10@gmail.com>
2025-03-16 14:44:11 +0500

    switch statement
    add switch statement for switching between the pages

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -55,6 +55,17 @@ class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    Widget page;
+switch (selectedIndex) {
+  case 0:
+    page = GeneratorPage();
+    break;
+  case 1:
+    page = Placeholder();
+    break;
+  default:
+    throw UnimplementedError('no widget for $selectedIndex');
+}
     return Scaffold(
       body: Row(
         children: [
